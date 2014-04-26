e0d4fef837c36bb01d99c2063d65188f91ae2031
aymenj <ay.j@hotmail.fr>
2014-04-26 17:57:04 -0400

    Added train/test data + Started R Code
    datasets are in the "Data" folder, my code is in the "Code" folder

---
+++ Code/Code-Aymen.R
@@ -0,0 +1,8 @@
+#============================
+#Using GLMS to Classifiy Wine
+#============================
+
+
+#Download the Data
+train = read.csv('train.csv',header=T)
+test = read.csv('test.csv',header=T)
